c5883e7358f86b79cc4b44934204df212c5a044f
MAXIME JASPARD <maxime@MacBook-Air-de-MAXIME.local>
2024-03-22 13:55:11 +0100

    test_mqtt_2g is ok 2 dir

--- test_mqtt_2G/test_mqtt_2G.ino
+++ test_mqtt_2G/test_mqtt_2G.ino
@@ -72,6 +72,13 @@ const char* mqtt_server = "windspotbelgium.com";
 const int mqtt_port = 1883; // Default MQTT port
 const char* topic_data = "FBVL0/wind_data";
 const char* topic_calibration = "FBVL0/calibration";
+const char* topic_sleep = "FBVL0/sleep_awake";
+const char* topic_check = "FBVL0/check_sleep_read";
+
+
+boolean topicReceived = false; // Flag to indicate if the topic data has been received
+int counter = 0;
+
 
 #include <TinyGsmClient.h>
 #include <PubSubClient.h>
@@ -89,42 +96,49 @@ PubSubClient mqtt(client);
 
 // Variable to store incoming message
 String incomingMessage;
-int counter = 0;
+
 
 uint32_t lastReconnectAttempt = 0;
 
 
 void callback(char* topic, byte* payload, unsigned int length) {
   // Handle incoming messages if needed
-  Serial.print("Message arrived [");
+  Serial.println("--> Start callback---");
+  Serial.print("--> Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
   incomingMessage = ""; // Clear the previous one
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+    //Serial.print((char)payload[i]);
     incomingMessage += (char)payload[i];
   }
-  Serial.println();
+  
+	// Set the flag to indicate that the topic data has been received
+  topicReceived = true;
+  Serial.println("--> End callback----");
   // Assuming payload is a string, convert it to an integer
 
 }
 
 void reconnect() {
   // Loop until we're reconnected
+  Serial.println("--> Start reconnect---");
   while (!mqtt.connected()) {
-    Serial.print("Attempting MQTT connection...");
+    Serial.print("--> Attempting MQTT connection...");
     // Attempt to connect
     if (mqtt.connect("ESP32Client")) {
-      Serial.println("connected");
+      Serial.println("--> connected");
+      mqtt.subscribe(topic_sleep);
 
     } else {
-      Serial.print("failed, rc=");
+      Serial.print("--> failed, rc=");
       Serial.print(mqtt.state());
-      Serial.println(" try again in 5 seconds");
+      Serial.println("-->  try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
     }
   }
+  Serial.println("--> End reconnect----");
 }
 
 
@@ -190,66 +204,67 @@ void setup()
   mqtt.setServer(mqtt_server, mqtt_port);
   mqtt.setCallback(callback);
 
+
+
 }
 
 void loop() {
 
-    
-  //--------------- WRITE MQTT data -------------------
-  // Read MQTT data
-  if (!mqtt.connected()) {
-    reconnect();
-  }
-  incomingMessage = "";
-  counter = 0;
-  SerialMon.print("read mqtt data...");
-  while ((incomingMessage == "") && (counter < 20)){
-    SerialMon.print(".");
-    mqtt.subscribe(topic_calibration);
-    mqtt.loop();
-    delay(500);   
-    counter++;   
-  }
-  SerialMon.println(".");
-  if (counter >= 20){
-    SerialMon.println("mqtt subscribe topic_calibration connection issue --> goes to sleep");
-  }
-    
-  SerialMon.print("inComing message = ");
-  SerialMon.println(incomingMessage);
+  Serial.println(""); 
+  Serial.println("--------- START LOOP---------");
   //--------------------------------------------------
   //--------------- WRITE MQTT data -------------------
   if (!mqtt.connected()) {
     reconnect();
   }
+  
+  mqtt.loop();
+
   // Convert sensor value to string
-  SerialMon.print("write mqtt data...");
+  SerialMon.println("write mqtt data...");
   counter = 0;
   boolean success = false;
-  String data = String("test1,rest2");
-  // Publish the data to the MQTT topic
-//  SerialMon.print("publish data to ");
-//  SerialMon.println(topic_data);
-//  SerialMon.print("data:  ");
-//  SerialMon.println(data.c_str());
- // Attempt to publish data
-   while ((success == false) && (counter < 20)){
-        SerialMon.print(".");
-        if (mqtt.connected()) {
-          success = mqtt.publish(topic_data, data.c_str());
-          if (success) {
-            Serial.println("Data published successfully");
-          } else {
-            Serial.print(".");
-          }
-        }
-        delay(1000);
-        counter++;
-  }
-  SerialMon.println(".");
-  if (counter >= 20){
-    SerialMon.println("mqtt publish topic_data connection issue --> goes to sleep");
-  }
+  String data_to_publish = "[0,0,0,0,0,0,50,45,0,0,0,0,0,0,0,0,45,10,25,2026,3.6,300]";
+  String data = String(data_to_publish);
+  SerialMon.println("wind_data publishing...");
+	mqtt.publish(topic_data, data.c_str(), true);
+
+	SerialMon.print("Check sleep info ...");
+  // Reset the flag and counter for the next iteration
+  topicReceived = false;
+	counter = 0;
+while ((!topicReceived) && (counter < 20)) {
+	  SerialMon.print(".");
+     if (!mqtt.connected()) {
+    reconnect();
+    }
+	  delay(1000);
+    // If not received, attempt to reconnect to MQTT and subscribe again
+    // reconnect();
+    mqtt.loop();   
+    counter++;
+  	if (topicReceived){
+    	// If received, continue with your program logic
+   		 Serial.println("Sleep info received successfully.");
+    
+    	// Your program logic here
+    
+
+ 	 }
+ } 
+	if (counter >= 20){
+		Serial.println("Sleep info received FAILED .");
+    
+    	// Your program logic here
+	}
+	
+  //mqtt.loop();
+//	mqtt.subscribe(topic_sleep, 2);
+  SerialMon.print("inComing message = ");
+  SerialMon.println(incomingMessage);
+
+
+//--------------- READ MQTT data -------------------
 
 
 
@@ -257,5 +272,6 @@ void loop() {
   
   // Wait for some time before publishing again
   SerialMon.println("wait 30s...");
-  delay(30000); // Adjust delay as needed
+  delay(300000); // Adjust delay as needed
+
 }
